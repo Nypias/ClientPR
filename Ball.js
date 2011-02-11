b963205fb74ae20f1919a210167e1ae5cd52499b
mathieu <sabourin.mathieu@free.fr>
2011-02-12 00:04:15 +0100

    Added class Ball.

---
+++ Ball.js
@@ -0,0 +1,16 @@
+function Ball(radius, x, y){
+    this.radius = radius;
+    this.x = x;
+    this.y = y;
+
+    this.draw = function(ctx){
+	ctx.beginPath();
+	ctx.arc(this.x, this.y, radius, 0, Math.PI*2, true);
+	ctx.fill();
+    };
+
+    this.moveTo(x, y){
+	this.x = x;
+	this.y = y;
+    };
+}
