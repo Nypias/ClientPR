1df9e9b946df89a08e2e70dce4b148866c65cec7
mathieu <sabourin.mathieu@free.fr>
2011-02-11 20:38:00 +0100

    Rotate know draws objects at expected place.

--- Scene.js
+++ Scene.js
@@ -8,7 +8,7 @@ function Scene() {
 	    this.components[name] = comp;
 	else
 	    return;
-    }
+    };
 
     this.drawAll = function()
     {
@@ -16,25 +16,26 @@ function Scene() {
 	{
 	    this.components[i].draw(this.ctx);
 	}
-    }
+    };
 
     this.clear = function(){
-	alert(this.canvas.width +" "+ this.canvas.height);
-	
 	this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); 
-    }
+    };
 
     this.rotate = function(x, y, angle){
-	this.ctx.save();
 	this.clear();
+	this.ctx.save();
 	this.ctx.translate(x, y);
 	this.ctx.rotate(angle);
+	this.ctx.translate(-x, -y);
 	this.drawAll();
 	this.ctx.restore();
-    }
+    };
 
     this.attachCanvas = function(name){
 	this.canvas = document.getElementById(name);
 	this.ctx = this.canvas.getContext('2d');
-    }
+	this.ctx = document.getElementById(name).getContext('2d');
+    };
+
 }
