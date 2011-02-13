1fc5bee331ed5d2bdef1a473368fbe5893412eca
mathieu <sabourin.mathieu@free.fr>
2011-02-13 20:23:42 +0100

    Created PongZone.

---
+++ PongZone.js
@@ -0,0 +1,12 @@
+function PongZone(array){
+    this.sides = array;
+
+    this.draw = function(ctx){
+	for (var i in this.sides){
+	    ctx.beginPath();
+	    ctx.moveTo(this.sides[i][0][0], this.sides[i][0][1]);
+	    ctx.lineTo(this.sides[i][1][0], this.sides[i][1][1]);
+	    ctx.stroke();
+	}
+    };
+}
