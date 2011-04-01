f6721ffe4309497e6796cf40fda02979143125f7
Nypias <nypias@gmail.com>
2011-04-01 13:51:53 +0200

    Mise a jour de Game.js

--- Game.js
+++ Game.js
@@ -28,6 +28,29 @@ function Game {
 	{
 		this.tabJoueurs = tabJoueurs;
 	}
+	
+	this.game = function()
+	{
+		Ball(DIMENSION_BALLE, this.ball.x, this.ball.y, true);
+		this.ancres = array;
+		
+		for (var i in this.tabJoueurs.nom)
+		{
+			ancres[i][0][0] = this.tabJoueurs.nom[i].ancre1.x * CANVAS_WIDTH;
+			ancres[i][0][1] = this.tabJoueurs.nom[i].ancre1.y * CANVAS_HEIGHT;
+			ancres[i][1][0] = this.tabJoueurs.nom[i].ancre2.x * CANVAS_WIDTH;
+			ancres[i][1][1] = this.tabJoueurs.nom[i].ancre2.y * CANVAS_HEIGHT;
+			
+			ax = ((this.tabJoueurs.nom[i].ancre2.x - this.tabJoueurs.nom[i].ancre1.x) * this.tabJoueurs.nom[i].pos / 100 + this.tabJoueurs.nom[i].ancre1.x) * CANVAS_WIDTH;
+			ay = ((this.tabJoueurs.nom[i].ancre2.y - this.tabJoueurs.nom[i].ancre1.y) * this.tabJoueurs.nom[i].pos / 100 + this.tabJoueurs.nom[i].ancre1.y) * CANVAS_HEIGHT;
+			bx = Math.sqrt(Math.pow(ax + ancres[i][0][0] - ancres[i][1][0], 2)) * 0.10;
+			by = Math.sqrt(Math.pow(ay + ancres[i][0][1] - ancres[i][1][1], 2)) * 0.10;
+			
+			Slider(ax, ay, bx, by);
+		}
+		
+		PongZone(ancres);
+	}
 
 
 	nw.events.onSyncJ = this.setJoueurs;
