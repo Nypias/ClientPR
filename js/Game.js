d19cde5d8a8671254cacc382e8b20ede35b2a634
atamborrino <al.tamborrino@gmail.com>
2011-05-20 00:36:55 +0200

    Augmentation vitesse raquette + petit bug corrigé sur extrémité

--- js/Game.js
+++ js/Game.js
@@ -353,10 +353,10 @@ function Game(nomJoueur) {
   this.moveSlider = function(slider, pos){	
     
     
-    if (pos > 10 && pos < 90)
+    if (pos > 9 && pos < 91)
     {
         //console.log("Position : " + pos);
-        slider.changePosition(pos); 
+        slider.changePosition(pos);
         this.nw.sendBouge(pos);
     }
     
@@ -369,7 +369,7 @@ function Game(nomJoueur) {
     
   this.moveSliderServer = function(slider, pos){	
      
-    if (pos > 10 && pos < 90)
+    if (pos > 9 && pos < 91)
     {
         slider.changePosition(pos); 
     }
